21e657d8449813ad41419cde6f5242e833148a73
richardchong999 <richardchong99@live.com>
2024-09-16 22:37:11 -0600

    Update hello-openshift-for-developers.go
    T240916 10:36pm

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World! T240915 1146pm###"
+		response = "Hello World! T240916 1036pm###"
 	}
 
 	fmt.Fprintln(w, response)
